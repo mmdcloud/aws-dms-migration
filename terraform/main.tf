14249387afd13a50e272f83677a3bf0af19ce593
mmdcloud <madmaxcloudpractice@gmail.com>
2025-05-31 16:18:08 +0530

    update: added aws and gcp db config

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,37 +11,44 @@ data "vault_generic_secret" "cloudsql" {
 # ------------------------ GCP Configuration ------------------------
 
 # VPC Creation
-module "carshub_vpc" {
-  source                  = "../../modules/network/vpc"
+module "source_vpc" {
+  source                  = "./modules/gcp/network/vpc"
   auto_create_subnetworks = false
-  vpc_name                = "carshub-vpc"
+  vpc_name                = "source-vpc"
   routing_mode            = "REGIONAL"
 }
 
 # Subnets Creation
-module "carshub_public_subnets" {
-  source                   = "../../modules/network/subnet"
-  name                     = "carshub-public-subnet"
+module "source_vpc_public_subnets" {
+  source                   = "./modules/gcp/network/subnet"
+  name                     = "source-public-subnet"
   subnets                  = var.public_subnets
-  vpc_id                   = module.carshub_vpc.vpc_id
+  vpc_id                   = module.source_vpc.vpc_id
   private_ip_google_access = false
   location                 = var.location
 }
 
-module "carshub_private_subnets" {
-  source                   = "../../modules/network/subnet"
-  name                     = "carshub-private-subnet"
+module "source_vpc_private_subnets" {
+  source                   = "./modules/gcp/network/subnet"
+  name                     = "source-private-subnet"
   subnets                  = var.private_subnets
-  vpc_id                   = module.carshub_vpc.vpc_id
+  vpc_id                   = module.source_vpc.vpc_id
   private_ip_google_access = true
   location                 = var.location
+}
+
+# Secret Manager
+module "source_cloudsql_password_secret" {
+  source      = "./modules/gcp/secret-manager"
+  secret_data = tostring(data.vault_generic_secret.cloudsql.data["password"])
+  secret_id   = "source_db_password_secret"
 }
 
 # Cloud SQL
 module "source_db" {
   source                      = "./modules/gcp/cloud-sql"
   name                        = "source-db"
-  db_name                     = "carshub"
+  db_name                     = "source-db"
   db_user                     = "mohit"
   db_version                  = "MYSQL_8_0"
   location                    = var.location
@@ -51,38 +58,38 @@ module "source_db" {
   disk_size                   = 10
   deletion_protection_enabled = false
   backup_configuration        = []
-  vpc_self_link               = module.carshub_vpc.self_link
-  vpc_id                      = module.carshub_vpc.vpc_id
-  password                    = module.carshub_sql_password_secret.secret_data
-  depends_on                  = [module.carshub_sql_password_secret]
+  vpc_self_link               = module.source_vpc.self_link
+  vpc_id                      = module.source_vpc.vpc_id
+  password                    = module.source_cloudsql_password_secret.secret_data
+  depends_on                  = [module.source_cloudsql_password_secret]
   database_flags              = []
 }
 
 # ------------------------ AWS Configuration ------------------------
 
 # VPC Configuration
-module "carshub_vpc" {
-  source                = "../../modules/vpc/vpc"
-  vpc_name              = "carshub_vpc_${var.env}"
+module "destination_vpc" {
+  source                = "./modules/aws/vpc/vpc"
+  vpc_name              = "destination-vpc"
   vpc_cidr_block        = "10.0.0.0/16"
   enable_dns_hostnames  = true
   enable_dns_support    = true
-  internet_gateway_name = "carshub_vpc_igw_${var.env}"
+  internet_gateway_name = "destination_vpc_igw"
 }
 
 # RDS Security Group
-module "carshub_rds_sg" {
-  source = "../../modules/vpc/security_groups"
-  vpc_id = module.carshub_vpc.vpc_id
-  name   = "carshub_rds_sg_${var.env}"
+module "destination_rds_sg" {
+  source = "./modules/aws/vpc/security_groups"
+  vpc_id = module.destination_vpc.vpc_id
+  name   = "destination_rds_sg"
   ingress = [
     {
       from_port       = 3306
       to_port         = 3306
       protocol        = "tcp"
       self            = "false"
-      cidr_blocks     = []
-      security_groups = [module.carshub_ecs_backend_sg.id]
+      cidr_blocks     = ["0.0.0.0/0"]
+      security_groups = []
       description     = "any"
     }
   ]
@@ -97,9 +104,9 @@ module "carshub_rds_sg" {
 }
 
 # Public Subnets
-module "carshub_public_subnets" {
-  source = "../../modules/vpc/subnets"
-  name   = "carshub public subnet_${var.env}"
+module "destination_public_subnets" {
+  source = "./modules/aws/vpc/subnets"
+  name   = "destination public subnet"
   subnets = [
     {
       subnet = "10.0.1.0/24"
@@ -114,14 +121,14 @@ module "carshub_public_subnets" {
       az     = "us-east-1c"
     }
   ]
-  vpc_id                  = module.carshub_vpc.vpc_id
+  vpc_id                  = module.destination_vpc.vpc_id
   map_public_ip_on_launch = true
 }
 
 # Private Subnets
-module "carshub_private_subnets" {
-  source = "../../modules/vpc/subnets"
-  name   = "carshub private subnet_${var.env}"
+module "destination_private_subnets" {
+  source = "./modules/aws/vpc/subnets"
+  name   = "destination private subnet"
   subnets = [
     {
       subnet = "10.0.6.0/24"
@@ -136,39 +143,39 @@ module "carshub_private_subnets" {
       az     = "us-east-1f"
     }
   ]
-  vpc_id                  = module.carshub_vpc.vpc_id
+  vpc_id                  = module.destination_vpc.vpc_id
   map_public_ip_on_launch = false
 }
 
-# Carshub Public Route Table
-module "carshub_public_rt" {
-  source  = "../../modules/vpc/route_tables"
-  name    = "carshub public route table_${var.env}"
-  subnets = module.carshub_public_subnets.subnets[*]
+# Destination Public Route Table
+module "destination_public_rt" {
+  source  = "./modules/aws/vpc/route_tables"
+  name    = "destination public route table"
+  subnets = module.destination_public_subnets.subnets[*]
   routes = [
     {
       cidr_block     = "0.0.0.0/0"
-      gateway_id     = module.carshub_vpc.igw_id
+      gateway_id     = module.destination_vpc.igw_id
       nat_gateway_id = ""
     }
   ]
-  vpc_id = module.carshub_vpc.vpc_id
+  vpc_id = module.destination_vpc.vpc_id
 }
 
-# Carshub Private Route Table
-module "carshub_private_rt" {
-  source  = "../../modules/vpc/route_tables"
-  name    = "carshub public route table_${var.env}"
-  subnets = module.carshub_private_subnets.subnets[*]
-  routes = []
-  vpc_id = module.carshub_vpc.vpc_id
+# Destination Private Route Table
+module "destination_private_rt" {
+  source  = "./modules/aws/vpc/route_tables"
+  name    = "destination public route table"
+  subnets = module.destination_private_subnets.subnets[*]
+  routes  = []
+  vpc_id  = module.destination_vpc.vpc_id
 }
 
 # Secrets Manager
-module "carshub_db_credentials" {
-  source                  = "../../modules/secrets-manager"
-  name                    = "carshub_rds_secrets_${var.env}"
-  description             = "carshub_rds_secrets_${var.env}"
+module "destination_db_credentials" {
+  source                  = "./modules/aws/secrets-manager"
+  name                    = "destination_rds_secrets"
+  description             = "destination_rds_secrets"
   recovery_window_in_days = 0
   secret_string = jsonencode({
     username = tostring(data.vault_generic_secret.rds.data["username"])
@@ -188,14 +195,14 @@ module "destination_db" {
   parameter_group_name    = "default.mysql8.0"
   username                = tostring(data.vault_generic_secret.rds.data["username"])
   password                = tostring(data.vault_generic_secret.rds.data["password"])
-  subnet_group_name       = "carshub_rds_subnet_group"
+  subnet_group_name       = "destination_rds_subnet_group"
   backup_retention_period = 7
   backup_window           = "03:00-05:00"
   subnet_group_ids = [
-    module.carshub_public_subnets.subnets[0].id,
-    module.carshub_public_subnets.subnets[1].id
+    module.destination_public_subnets.subnets[0].id,
+    module.destination_public_subnets.subnets[1].id
   ]
-  vpc_security_group_ids = [module.carshub_rds_sg.id]
+  vpc_security_group_ids = [module.destination_rds_sg.id]
   publicly_accessible    = false
   skip_final_snapshot    = true
 }
