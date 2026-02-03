f34288b299894f80933b47f8d0fdc65ea4a8179c
mmdcloud <madmaxcloudpractice@gmail.com>
2026-02-03 20:08:54 +0530

    update: added tfvars file

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,8 @@
+source_location            = "us-central1"
+destination_location       = "us-east-1"
+source_db                  = "source-db"
+destination_db             = "destinationdb"
+destination_public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+destination_private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+destination_database_subnets = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
+destination_azs            = ["us-east-1a", "us-east-1b", "us-east-1c"]
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,43 +1,35 @@
 variable "source_location" {
-  type    = string
-  default = "us-central1"
+  type = string
 }
 
 variable "destination_location" {
-  type    = string
-  default = "us-east-1"
+  type = string
 }
 
 variable "source_db" {
-  type    = string
-  default = "source-db"
+  type = string
 }
 
 variable "destination_db" {
-  type    = string
-  default = "destinationdb"
+  type = string
 }
 
 variable "destination_public_subnets" {
   type        = list(string)
   description = "Public Subnet CIDR values"
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "destination_private_subnets" {
   type        = list(string)
   description = "Private Subnet CIDR values"
-  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
 
 variable "destination_database_subnets" {
   type        = list(string)
   description = "Private Subnet CIDR values"
-  default     = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
 }
 
 variable "destination_azs" {
   type        = list(string)
   description = "Availability Zones"
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
