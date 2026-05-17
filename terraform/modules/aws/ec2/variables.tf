37b82f4c10afffd9d2ca6cdb5adb968fddb0b288
mmdcloud <madmaxcloudpractice@gmail.com>
2026-05-17 07:36:02 +0530

    feat: Update variable definitions in variables.tf and terraform.tfvars.example for AWS DMS migration

--- terraform/modules/aws/ec2/variables.tf
+++ terraform/modules/aws/ec2/variables.tf
@@ -2,7 +2,10 @@ variable "name"{}
 variable "ami"{}
 variable "instance_type"{}
 variable "associate_public_ip_address"{}
-variable "key_name"{}
+variable "key_name"{
+    type = string
+    default = ""
+}
 variable "subnet_id"{}
 variable "security_groups"{}
 variable "user_data"{}
